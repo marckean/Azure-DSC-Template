4a6172161d0b75fec0254c2c58f5ef8fe24b46e5
Marc Kean <marc@marckean.com>
2019-05-15 14:37:15 +1000

    .

--- Deploy-Azure.ps1
+++ Deploy-Azure.ps1
@@ -26,6 +26,7 @@ $DSCSourceFolder = $ArtifactStagingDirectory + '\DSC'
 $DebugOptions = "None"
 $StorageAccountName = 'stage' + ((Get-AzureRmContext).Subscription.Id).Replace('-', '').substring(0, 19)
 # Existing DSC source files storage account, where DSC source files are stored on blob storage
+$dscSourceFilesLocationName = 'DSCStorage'
 $dscStorageAccountName = 'marcdsc2019' # Set this to blank '' to skip using DSC source files
 $dscStorageContainerName = 'ejukeartifacts'
 
